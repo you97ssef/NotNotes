2d384f98456507f8e3b9d1e5dd204694d58f35fc
Youssef BAHI <you97ssef@gmail.com>
2024-03-21 23:34:10 +0100

    Configured Hive on model

--- lib/features/notes/data/models/note_model.dart
+++ lib/features/notes/data/models/note_model.dart
@@ -1,15 +1,61 @@
+import 'package:hive/hive.dart';
+import 'package:not_notes/core/util/util.dart';
 import 'package:not_notes/features/notes/domain/entities/note_entity.dart';
 
-class NoteModel extends NoteEntity {
-  NoteModel({required super.id, required super.title, required super.content, required super.createdTime, required super.updatedTime});
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject implements NoteEntity {
+  @override
+  @HiveField(0)
+  final String id;
+
+  @override
+  @HiveField(1)
+  final String title;
+
+  @override
+  @HiveField(2)
+  final String content;
+
+  @override
+  @HiveField(3)
+  final DateTime createdTime;
+
+  @override
+  @HiveField(4)
+  final DateTime updatedTime;
+
+  NoteModel({required this.id, required this.title, required this.content, required this.createdTime, required this.updatedTime});
 
   factory NoteModel.fromJson(Map<String, dynamic> json) {
     return NoteModel(
-      id: json['id'],
-      title: json['title'],
-      content: json['content'],
+      id: json['id'].toString(),
+      title: json['title'].toString(),
+      content: json['content'].toString(),
       createdTime: DateTime.parse(json['createdTime']),
       updatedTime: DateTime.parse(json['updatedTime']),
+    );
+  }
+
+  factory NoteModel.fresh({required String title, required String content}) {
+    var now = DateTime.now();
+    return NoteModel(
+      id: Util.randomString(10),
+      title: title,
+      content: content,
+      createdTime: now,
+      updatedTime: now,
+    );
+  }
+
+  factory NoteModel.refresh(NoteEntity entity, {required String title, required String content}) {
+    return NoteModel(
+      id: entity.id,
+      title: title,
+      content: content,
+      createdTime: entity.createdTime,
+      updatedTime: DateTime.now(),
     );
   }
 }
---
+++ lib/features/notes/data/models/note_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      content: fields[2] as String,
+      createdTime: fields[3] as DateTime,
+      updatedTime: fields[4] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.content)
+      ..writeByte(3)
+      ..write(obj.createdTime)
+      ..writeByte(4)
+      ..write(obj.updatedTime);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
